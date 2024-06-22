6857539eef5e71eb60ee2fb6ee18716ec75f9c0d
Jake <jacobminor@hotmail.co.uk>
2024-06-22 15:01:38 +0100

    Remove portfolio controller and combine with investor controller

--- api/src/main/java/investment/api/controllers/InvestorController.java
+++ api/src/main/java/investment/api/controllers/InvestorController.java
@@ -1,13 +1,12 @@
 package investment.api.controllers;
 
 import investment.api.business.InvestorBusiness;
+import investment.api.business.PortfolioBusiness;
+import investment.api.dtos.CreatePortfolioDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/investor/")
@@ -16,13 +15,21 @@ public class InvestorController {
     @Autowired
     private InvestorBusiness investorBusiness;
 
+    @Autowired
+    private PortfolioBusiness portfolioBusiness;
+
     @DeleteMapping("/delete-profile")
     public ResponseEntity deleteInvestor(Authentication authentication) {
         return investorBusiness.deleteInvestor(authentication);
     }
 
-    @GetMapping("/")
+    @GetMapping("/get-profile")
     public ResponseEntity getInvestor(Authentication authentication) {
         return investorBusiness.getInvestorProfile(authentication);
+    }
+
+    @PostMapping("/create-portfolio")
+    public ResponseEntity createPortfolio(@RequestBody CreatePortfolioDto portfolio, Authentication authentication) {
+        return portfolioBusiness.createPortfolio(portfolio, authentication);
     }
 }
