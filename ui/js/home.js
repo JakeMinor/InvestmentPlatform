d57145530b74a03f88341d98b29ca242b2e423ba
Jake <jacobminor@hotmail.co.uk>
2024-06-22 20:30:10 +0100

    refactor router so that it is a little simpler

---
+++ ui/js/home.js
@@ -0,0 +1,3 @@
+document.getElementById('home-script').onload = function () {
+    console.log("home")
+}
--- ui/js/portfolio.js
+++ ui/js/portfolio.js
@@ -1,3 +1,3 @@
 document.getElementById('portfolio-script').onload = function () {
-    console.log("hello")
+    console.log("portfolio")
 }
